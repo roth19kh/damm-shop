9b1abd0ed2924b83fb55627eb016a49b1b944823
roth19 <haktharoth18@gmail.com>
2025-10-31 01:17:24 +0700

    lastlast

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 from flask import Flask, render_template, jsonify, abort, request, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-import requests
-from flask_mail import Mail, Message
-import os
-from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://roth:x7Y7FALpSmfl5nS1iyMyf4zUA8lEq8FL@dpg-d41q71jipnbc73bsk85g-a.oregon-postgres.render.com/flask_lnt7'
 app.config['SECRET_KEY'] = 'your-secret-key-change-this'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_recycle': 300,
+    'pool_pre_ping': True
+}
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Email configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'haktharoth18@gmail.com'
-app.config['MAIL_PASSWORD'] = 'bjbu yxgl hixz wriu'
-
-mail = Mail(app)
-
-# Your existing routes and models will work again
-
 
 import models
-
 import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
