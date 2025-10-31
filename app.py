67f3522d7317f3dfb218fc1e538b8a62cb6e11a5
roth19 <haktharoth18@gmail.com>
2025-10-31 08:44:32 +0700

    somvor

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://roth:x7Y7FALpSmfl5nS1iyMyf4zUA8lEq8FL@dpg-d41q71jipnbc73bsk85g-a.oregon-postgres.render.com/flask_lnt7'
-app.config['SECRET_KEY'] = 'your-secret-key-change-this'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://roth:x7Y7FALpSmfl5nS1iyMyf4zUA8lEq8FL@dpg-d41q71jipnbc73bsk85g-a.oregon-postgres.render.com/flask_lnt7'
+app.config['SECRET_KEY'] = '5bae2b11e01ff0c318ed744434506229b46a298c7e7ef52a2f3bb171130c65cf'
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_recycle': 300,
     'pool_pre_ping': True
